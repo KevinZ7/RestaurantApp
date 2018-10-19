a9986bb3e968907ddcf808b259aacc200fbdcfde
Andrea <andreamayhirji@gmail.com>
2018-10-19 22:50:53 +0000

    renamed styles in BEM format, started app.js to build up rendering donut data to page.

---
+++ app.js
@@ -0,0 +1,10 @@
+// Render Dount Card to page
+
+//header data
+let $headerContent = $('<header>');
+let $donutImg = $('<img>').attr("src", ...).addClass('card-display');
+//compile header
+$headerContent.append($donutImg);
+
+//section data
+let $donutContent
