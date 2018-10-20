1f551a9d223262932c28bd07b805b62a16167524
Andrea <andreamayhirji@gmail.com>
2018-10-20 00:27:51 +0000

    updated cart class names and worked on tables for cart and admin page.

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-// Render Dount Card to page
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
-//header data
-let $headerContent = $('<header>');
-let $donutImg = $('<img>').attr("src", ...).addClass('card-display');
-//compile header
-$headerContent.append($donutImg);
+$(document).ready(function () {
 
-//section data
-let $donutContent
+    function createDonutCard(...donutData...) {
+
+        // Render Dount Card to page
+
+
+        //header data
+        let $headerContent = $('<header>');
+        let $donutImg = $('<img>').attr("src", ...).addClass('card-display');
+        //compile header
+        $headerContent.append($donutImg);
+
+        //section data
+        let $donutContent = $('<section>').addClass('donut-content');
+        let $donutName = $('<h3>').text(...).addClass('');
+        let $donutDescription = $('<p>').text(...).addClass('');
+        let $donutPrice = $('<span>').text(...).addClass('');
+        //compile section
+        $donutContent.append($donutName, $donutDescription, $donutPrice);
+
+        //bring together
+        let $donutCard = $('<article>').addClass('donut-card');
+        $donutCard.append($donutImg, $donutContent);
+
+        return $donutCard;
+
+        function renderDonutCardToPage(donuts) {
+            donuts.forEach(function (donut) {
+                $("#...").prepend(createDonutCard(donut));
+            })
+        }
+
+    }
+
+});
