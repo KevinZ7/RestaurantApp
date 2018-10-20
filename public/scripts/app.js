156c13edad4ea31cb31e5f2c20dcfe473f37f0c3
David <david.khayutin@gmail.com>
2018-10-20 19:55:33 +0000

    changes to js and body structure

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
   function createMenuBody(menuItem){
-
-    let $headerContent = $('<header>');
-    let $donutImg = $('<img>').attr("src", menuItem.avatar).addClass('donut-card__img');
-    let $donutContent = $('<section>').addClass('donut-card__body-text');
-    let $donutName = $('<h3>').text(menuItem.name).addClass('donut-card__name');
-    let $donutDescription = $('<p>').text(menuItem.description).addClass('donut-card__description');
-    let $donutPrice = $('<span>').text(menuItem.price).addClass('donut-card__price');
-    let $donutCard = $('<article>').addClass('donut-card');
-
-    $headerContent.append($donutImg);
-    $donutContent.append($donutName, $donutDescription, $donutPrice);
-    $donutCard.append($donutImg, $donutContent);
-
-    return $donutCard;
+    return $('<article>').addClass("donut-card")
+      .append($('<header>').append($('<img>').attr({src: menuItem.avatar}).addClass('donut-card__img')))
+      .append($('<section>').addClass('donut-card__body-text')
+        .append($('<h3>').text(menuItem.name).addClass('donut-card__name'))
+        .append($('<p>').text(menuItem.description).addClass('donut-card__description'))
+        .append($('<span>').text(`$${(Number(menuItem.price)/100)}`).addClass('donut-card__price'))
+        .append($('<br>')).append($('<a>').addClass('donut-card__btn').attr({
+          id : menuItem.id,
+          href: '#',
+          'data-toggle' : 'modal',
+          'data-target': '#cartModal',
+          'target': '_blank'
+        }).text('Add to Cart')))
   }
 
   function renderMenu(data){
     data.forEach((menuItem) =>{
-      $('.itemsInMenu').append(createMenuBody(menuItem))
+      $('.card-layout').append(createMenuBody(menuItem))
     })
   }
 
   function loadMenu(){
     $.ajax('/items').then((response) =>{
+      console.log(response)
       renderMenu(response.menu);
     })
   }
@@ -34,8 +34,9 @@ $(document).ready(function(){
 
 
 
-  $(".orderButtons").click((event) => {
+  $('.card-layout').on('click','.donut-card__btn',(event) => {
     let itemId = event.target.id;
+    console.log(itemId);
 
     $.ajax('/addToCart', {
       method: "POST",
@@ -43,7 +44,7 @@ $(document).ready(function(){
         item_id: itemId
       },
       success: (val) => {
-        $('p').remove();
+        $('.table').remove();
         $('footer').remove();
         var cart = val.cart;
         var total = 0;
@@ -54,18 +55,20 @@ $(document).ready(function(){
             .append($('<tr>')
               .append($('<td>').addClass('name').text(cartItem.name))
               .append($('<td>').addClass('qty').text(`QTY:${cartItem.count}`))
-              .append($('<td>').addClass('price').text(`Price: $${cartItem.price}`))
+              .append($('<td>').addClass('price').text(`Price: $${(Number(cartItem.price))/100}`))
+              .append($('<button>').attr('id', `minus_${cartItem.id}`).addClass('deleteOne').text("delete"))
+              .append($('<button>').attr('id', `plus_${cartItem.id}`).addClass('addOne').text("add"))
               ))
            })
 
-        $('div').append($('<footer>').text(`price: $ ${total}`));
+        $('.modal-body').append($('<footer>').text(`price: $ ${total}`));
 
       }
     })
   });
 
-  $('.cart').on('click','.deleteButton',function(event){
-    let itemId = event.target.id.slice(7);
+  $('.modal-body').on('click','.deleteOne',function(event){
+    let itemId = event.target.id.slice(6);
 
     $.ajax('/removeFromCart', {
       method: "POST",
@@ -75,7 +78,7 @@ $(document).ready(function(){
       success:(val) => {
 
         console.log(val);
-        $('p').remove();
+        $('.table').remove();
         $('footer').remove();
         var cart = val.cart;
         var total = 0;
@@ -88,6 +91,41 @@ $(document).ready(function(){
               .append($('<td>').addClass('name').text(cartItem.name))
               .append($('<td>').addClass('qty').text(`QTY:${cartItem.count}`))
               .append($('<td>').addClass('price').text(`Price: $${cartItem.price}`))
+              .append($('<button>').attr('id', `minus_${cartItem.id}`).addClass('deleteOne').text("delete"))
+              .append($('<button>').attr('id', `plus_${cartItem.id}`).addClass('addOne').text("add"))
+              ))
+           })
+        $('.modal-body').append($('<footer>').text(`price: $ ${total}`));
+        }
+      })
+  });
+
+   $('.modal-body').on('click','.addOne',function(event){
+    let itemId = event.target.id.slice(5);
+
+    $.ajax('/addToCart', {
+      method: "POST",
+      data: {
+        item_id: itemId
+      },
+      success:(val) => {
+
+        console.log(val);
+        $('.table').remove();
+        $('footer').remove();
+        var cart = val.cart;
+        var total = 0;
+
+        cart.forEach((cartItem, i) =>{
+          total += Number(cartItem.sum)/100;
+
+          $(".modal-body").append($('<table>').addClass('table')
+            .append($('<tr>')
+              .append($('<td>').addClass('name').text(cartItem.name))
+              .append($('<td>').addClass('qty').text(`QTY:${cartItem.count}`))
+              .append($('<td>').addClass('price').text(`Price: $${cartItem.price}`))
+              .append($('<button>').attr('id', `minus_${cartItem.id}`).addClass('deleteOne').text("delete"))
+              .append($('<button>').attr('id', `plus_${cartItem.id}`).addClass('addOne').text("add"))
               ))
            })
         $('.modal-body').append($('<footer>').text(`price: $ ${total}`));
@@ -96,19 +134,49 @@ $(document).ready(function(){
   });
 
 
-  $(".submitOrder").click((event) =>{
+
+  $("#submitOrder").click((event) =>{
+    $('.table').remove();
+    $('footer').remove();
     $.ajax('/addToOrder', {
       method: "POST",
     })
   })
 
+
+  function renderOrderBody(orderInfo){
+    return $('<div>').addClass('confirmOrder')
+    .append($('<p>').text(orderInfo.name))
+    .append($('<p>').text(orderInfo.phone))
+    .append($('<p>').text(orderInfo.customer_orders_id))
+    .append($('<p>').text(orderInfo.count))
+  }
+
+  function renderOrderData(data){
+    data.forEach((orderItem)=>{
+      $('section').append(renderOrderBody(orderItem))
+    })
+  }
+
+
+
 function orderPlaced(){
    $.ajax('/orderPlaced').then((data) =>{
-    console.log(data)
+   renderOrderData(data.order)
   })
  }
 
 orderPlaced();
+
+
+  // $('#confirmOrder').click((event) =>{
+  //   $.ajax('/confirmOrder', {
+  //     method: 'POST',
+  //     data:
+  //   }
+  //   })
+  // })
+
 
 });
 
