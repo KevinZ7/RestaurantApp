22842648baa0155735a3e9b33dbc3d354a5e0712
Andrea <andreamayhirji@gmail.com>
2018-10-22 06:34:19 +0000

    added js script to apply a scroll effect on anchor links within a page

---
+++ public/scripts/effects.js
@@ -0,0 +1,12 @@
+
+// Applies a scroll effect to anchor links on a page.
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
